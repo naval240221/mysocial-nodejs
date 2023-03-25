9bb971aa4f38c4aa0db261d3dbc3ff61464a0f23
Naval Kumawat <navalkumawat@Navals-MacBook-Air.local>
2023-03-25 23:53:19 +0530

    Added dependencies to capture logs, enabling cors, helmet to establish different headers to safeguard apis

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const cookieparser = require('cookie-parser');
 require('../config/database').connect();
 
 const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const routes = require('./routes')
 
@@ -12,6 +16,18 @@ const server = express();
 server.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 })
+// adding Helmet to enhance your Rest API's security
+server.use(helmet());
+
+// using bodyParser to parse JSON bodies into JS objects
+server.use(bodyParser.json());
+
+// enabling CORS for all requests
+server.use(cors());
+
+// adding morgan to log HTTP requests
+server.use(morgan('combined'));
+
 server.use(express.json());
 server.use(cookieparser());
 server.use('', routes)
